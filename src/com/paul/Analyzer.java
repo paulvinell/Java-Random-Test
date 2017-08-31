7b26bdad50661bf0bd80c45900b6b4d403c50c29
Paul Vinell <31391457+paulvinell@users.noreply.github.com>
2017-08-31 15:29:09 +0200

    first commit

---
+++ src/com/paul/Analyzer.java
@@ -0,0 +1,73 @@
+package com.paul;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Analyzer {
+
+	public final String s;
+	public ArrayList<Analysis> runs = new ArrayList<Analysis>();
+	
+	public Analyzer(String s) {
+		this.s = s;
+	}
+	
+	public Analysis run(int size) {
+		HashMap<String, Integer> h = new HashMap<String, Integer>();
+		
+		for (int i = size; i < s.length(); i += size) {
+			String c = s.substring(i - size, i);
+			if (h.containsKey(c)) {
+				h.put(c, h.get(c) + 1);
+			} else {
+				h.put(c, 1);
+			}
+		}
+		
+		return new Analysis(size, h);
+	}
+	
+	public Analysis getAnalysisBySize(int size) {
+		for (Analysis a : runs) {
+			if (size == a.size) {
+				return a;
+			}
+		}
+		return null;
+	}
+	
+	public class Analysis {
+		
+		public int size;
+		public HashMap<String, Integer> values = new HashMap<String, Integer>();
+		
+		private Analysis(int size, HashMap values) {
+			this.size = size;
+			this.values = values;
+			
+			runs.add(this);
+		}
+		
+		private double average() {
+			int total = 0;
+			
+			for (String key : values.keySet()) {
+				total += values.get(key);
+			}
+			
+			return (double) total / values.size();
+		}
+		
+		
+		public double averageRelDiff() {
+			double diff = 0;
+			double avg = average();
+			
+			for (String key : values.keySet()) {
+				diff += Math.abs(1 - (values.get(key) / avg));
+			}
+			
+			return (double) diff / values.size();
+		}
+	}
+}
